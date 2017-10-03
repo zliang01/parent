96505e045825a2e88edd04fea725f3172e95c903
张学伟 <@DESKTOP-VSDLGM7>
2017-10-03 09:39:11 +0800

    <zxw> <考生查询功能> <删除初始提交的target>

---
+++ zxw-demo-dao/src/main/java/com/zxw/demo/dao/common/IQueryExamineerDaoService.java
@@ -0,0 +1,9 @@
+package com.zxw.demo.dao.common;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IQueryExamineerDaoService {
+	  public List<Map<String, Object>>  queryExamineeInfo(Map<String,Object> params) throws Exception;
+
+}
